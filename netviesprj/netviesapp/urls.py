61d79cbcbffbcae6e01ce74b4b66bbccadf91059
T-Betrand <tebobertrand@gmail.com>
2023-10-27 22:12:54 +0100

    url for profile display

--- netviesprj/netviesapp/urls.py
+++ netviesprj/netviesapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, ProfileList
 
 app_name = 'netviesapp'
 
 
 urlpatterns = [
-    path('', Home.as_view(), name='Home')
+    path('', Home.as_view(), name='Home'),
+    path('profiles/', ProfileList.as_view(), name='profile-list')
 ]
