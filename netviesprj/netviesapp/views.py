3352d0a5226446eabab5da0d889ecf1a935e927b
T-Betrand <tebobertrand@gmail.com>
2023-10-28 13:11:33 +0100

    created view for watching movie/playing vide

--- netviesprj/netviesapp/views.py
+++ netviesprj/netviesapp/views.py
@@ -76,3 +76,20 @@ class MovieDetail(View):
             return render(request, 'moviedetail.html', context)
         except Movie.DoesNotExist:
             return redirect('netviesapp:profile-list')
+
+
+
+method_decorator(login_required, name='dispatch')
+class PlayMovie(View):
+    def get(self, request, movie_id, *args, **kwargs):
+        try:
+            movie = Movie.objects.get(uuid=movie_id)
+            movie = movie.video.values()
+            
+            context = {
+                'movie':list(movie)
+            }
+
+            return render(request, 'playmovie.html', context)
+        except Movie.DoesNotExist:
+            return redirect('netviesapp:profile-list')
